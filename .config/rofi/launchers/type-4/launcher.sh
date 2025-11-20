a9abdf4dea3f4115904388cb3ebe1ea54dd89b28
messieurslucky <rufus@mailfence.com>
2025-11-20 12:32:49 +0000

    Add cliphost rofi

--- .config/rofi/launchers/type-4/launcher.sh
+++ .config/rofi/launchers/type-4/launcher.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-
 dir="$HOME/.config/rofi/launchers/type-4"
 theme='style-2'
 
-## Run
-rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+mode=${1:-drun} 
+
+if [ "$mode" = "clipboard" ]; then
+    rofi \
+        -modi "clipboard:$HOME/.local/bin/cliphist-rofi-img" \
+        -show clipboard \
+        -show-icons \
+        -theme "${dir}/${theme}.rasi"
+else
+    rofi \
+        -show "$mode" \
+        -theme "${dir}/${theme}.rasi"
+fi
+
